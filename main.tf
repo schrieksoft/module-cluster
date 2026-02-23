8e9235a24ba5272c51af496c5ca0861ed5598673
Karl Schriek <karl.schriek@snapcd.io>
2026-02-23 12:10:06 +0100

    add upgrade_settings

--- main.tf
+++ main.tf
@@ -104,6 +104,12 @@ resource "azurerm_kubernetes_cluster_node_pool" "this" {
   priority                = each.value.priority
   eviction_policy         = each.value.priority == "Spot" ? each.value.eviction_policy : null
 
+  upgrade_settings {
+    drain_timeout_in_minutes      = 0
+    max_surge                     = "10%"
+    node_soak_duration_in_minutes = 0
+ }
+
   lifecycle {
     ignore_changes = [
       node_count //ignore changes on node count since autoscaler might change it
